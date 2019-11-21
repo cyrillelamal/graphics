6c66f75530f2fce443f560b780b3e3ee93b5f5a9
Cyrille <cyrillekossyguine@gmail.com>
2019-11-21 21:53:27 +0300

    l3 drafts

--- back_lab3.py
+++ back_lab3.py
@@ -1,75 +1,174 @@
-from tkinter import *
+from inspect import signature
+import tkinter as tk
+
+
+NUM_OF_STEPS = 100  # Function
+
+SCALE = (10, 10)  # Pixels scale
+
+EC_WIDTH = 320
+EC_HEIGHT = 240
+
+
+def example_parabola(x, a=1, b=0, c=0) -> float:
+    """Return function value for passed"""
+    return a * x**2 + b * x + c
+
+
+class CanvasForGraphic:
+    """Should be used as mixin"""
+    def __init__(self, frame, width, height, func, scale=None, axis_bias=5):
+        self.frame = frame
+
+        self.width = width
+        self.height = height
+
+        self.scale = (1, 1) if scale is None else scale
+
+        self.axis_bias = axis_bias
+        self.axis_width = 2
+
+        self._center_x = width // 2
+        self._center_y = height // 2
+
+        self.func = func
+
+        self._append_canvas()
+
+    def _append_canvas(self):
+        c = tk.Canvas(self.frame, width=self.width, height=self.height)
+        c.pack()
+        self._canvas = c
+
+    def draw_axis(self):
+        """Draw x and y axis"""
+        c = getattr(self, '_canvas')
+
+        # X axis
+        x_l = self.axis_bias
+        x_r = self.width - self.axis_bias
+        y = self._center_y
+        c.create_line(x_l, y, x_r, y, width=self.axis_width)
+        # Y axis
+        x = self._center_x
+        y_t = self.axis_bias
+        y_b = self.height - self.axis_bias
+        c.create_line(x, y_t, x, y_b, width=self.axis_width)
+
+    def draw_graphic(self, x_min, x_max, y_min=None, y_max=None):
+        """Draw the line forming graphic"""
+        self._canvas.delete('all')
+        self.draw_axis()
+
+        # Function step
+        h = (x_max - x_min) / NUM_OF_STEPS
+
+        # Center of the canvas
+        x_mid = self._center_x
+        y_mid = self._center_y
+
+        fx = x_min  # Functional x
+
+        x_scale, y_scale = self.scale
+
+        xp = x_mid + int(fx * x_scale)
+        yp = y_mid - int(self.func(fx) * y_scale)
+
+        while fx < x_max:
+            fx += h
+            xc = x_mid + int(fx * x_scale)
+            yc = y_mid - int(self.func(fx) * y_scale)
+            self._canvas.create_line(xp, yp, xc, yc, width=2)
+            xp = xc
+            yp = yc
+
+    def enclose(self, *args):
+        """Enclose the function bound to the instance with args"""
+        def curry(old_func):
+            sig = signature(old_func)
+            if len(sig.parameters) == 1:
+                return old_func
+
+            def new_func(x):
+                return old_func(x, *args)
+            return new_func
+        return curry(self.func)
 
 
 class GUI:
-    LABELS = ['A', 'B', 'C', 'x min', 'y min', 'x max', 'y max']
-    CANVAS_HEIGHT = 200
-    CANVAS_WIDTH = 400
+    LABEL_NAMES = ['A', 'B', 'C', 'x min', 'y min', 'x max', 'y max']
+    LABEL_PADDING = (3, 5)
 
-    H = 1
+    def __init__(self, title):
+        m = tk.Tk()
+        m.title(title)
+        self.__master = m
 
-    def __init__(self):
-        self.__main_window = Tk()
-        self.__main_window.title('ЛР №3. Косыгин К.С.')
-        self._build_base_interface()
-        self._bind_defaults()
+        self._build_interface()
+        self._bind_actions()
 
-    def _build_base_interface(self):
-        # User's input frame
-        self._frame_input = Frame(self.__main_window)
-        self._frame_input.pack()
+    def _build_interface(self):
+        """Frames in the main window"""
+        m = self.__master
+        fe = tk.Frame(m)  # Frame with entries
+        fe.pack()
+        setattr(self, '_frame_entries', fe)
+        fb = tk.Frame(m)  # Frame with button
+        fb.pack()
+        setattr(self, '_frame_button', fb)
+        fc = tk.Frame(m)
+        fc.pack()
+        setattr(self, '_frame_canvas', fc)
 
-        # Build user's input
-        for label in GUI.LABELS:
-            Label(self._frame_input, text=label).pack()
+        # Entries
+        padx, pady = GUI.LABEL_PADDING  # Const padding
+        ln = GUI.LABEL_NAMES
+        for label, row in zip(ln, range(len(ln))):
+            tk.Label(fe, text=label).grid(row=row, column=0, padx=padx, pady=pady)
 
-            field_name = '_entry_' + label.replace(' ', '_').lower()
-            entry = Entry(self._frame_input)
-            setattr(self, field_name, entry)
-            entry.pack()
+            entry_name = '_entry_' + label.replace(' ', '_').lower()
+            e = tk.Entry(fe)
+            e.grid(row=row, column=1)
+            setattr(self, entry_name, e)
 
         # Button
-        self._do_button = Button(self._frame_input, text='Dessiner')
-        self._do_button.pack()
+        self._button = tk.Button(fb, text='Dessiner')
+        self._button.pack()
 
-        # Output
-        self._figure_frame = Frame(self.__main_window)
-        self._figure_frame.pack()
+        # Canvas
+        self._canvas = CanvasForGraphic(
+            fc,
+            EC_WIDTH, EC_HEIGHT,
+            example_parabola,
+            scale=SCALE
+        )
+        self._canvas.draw_axis()
 
-        self._canvas = Canvas(self._figure_frame)
-        self._canvas.pack()
+    def _bind_actions(self):
+        self._button.bind('<Button-1>', self._redraw)
 
-        # X
-        self._canvas.create_line(0, GUI.CANVAS_HEIGHT//2, GUI.CANVAS_WIDTH, GUI.CANVAS_HEIGHT//2)
-        # Y
-        self._canvas.create_line(GUI.CANVAS_WIDTH//2, 0, GUI.CANVAS_WIDTH//2, GUI.CANVAS_HEIGHT)
+    def _redraw(self, event):  # When 'redraw' button is pressed
+        """Redraw"""
+        user_input = self.get_user_input()
+        canvas = self._canvas
+        a = user_input.pop('a')
+        b = user_input.pop('b')
+        c = user_input.pop('c')
 
-    def _bind_defaults(self):
-        self._do_button.bind('<Button-1>', self.draw)
+        # Make closure with default function
+        canvas.enclose(a, b, c)
+        canvas.draw_graphic(**user_input)
 
-    @staticmethod
-    def func(x, a, b, c):
-        return a * x ** 2 + b * x + c
-
-    def draw(self, event):
-        values = {}
-        for label in GUI.LABELS:
-            key = label.replace(' ', '_').lower()
-            attr = '_entry_' + key
-            values[key] = float(getattr(self, attr).get())
-
-        a = values['a']
-        b = values['b']
-        c = values['c']
-        x_max = values['x_max']
-
-        x = values['x_min']
-        while x < x_max:
-            y = GUI.func(x, a, b, c)
-            x2 = x + GUI.H
-            y2 = GUI.func(x2, a, b, c)
-            self._canvas.create_line(x, y, x2, y2)
-            x = x2
+    def get_user_input(self) -> dict:
+        """Read inputs"""
+        return {
+            'a': float(getattr(self, '_entry_a').get()),
+            'b': float(getattr(self, '_entry_b').get()),
+            'c': float(getattr(self, '_entry_c').get()),
+            'x_min': float(getattr(self, '_entry_x_min').get()),
+            'x_max': float(getattr(self, '_entry_x_max').get()),
+            # y_max, y_min, etc
+        }
 
     def start_loop(self):
-        self.__main_window.mainloop()
+        self.__master.mainloop()
--- l3.py
+++ l3.py
@@ -1,5 +1,5 @@
 from back_lab3 import GUI
 
 
-g = GUI()
+g = GUI('ЛР №3. Косыгин К.С.')
 g.start_loop()
