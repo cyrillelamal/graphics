e76d9bcbac6c3bfa3da1c00ce95bd81efa9cfdc1
Lambda_man <cyrillekossyguine@outlook.fr>
2019-12-05 16:21:10 +0300

    Dithering for personal work #1

--- dithering.py
+++ dithering.py
@@ -1,58 +1,32 @@
-from math import floor
-
-
 def fs_dithering(pixels):
     """Floyd-Steiner dithering. Affect passed pixels"""
-    height = len(pixels)
-    width = len(pixels[0])
+    height = len(pixels) - 1
 
-    for y in range(1, height):
-        for x in range(1, width):
+    for y in range(height):
+        width = len(pixels[y]) - 1
+        for x in range(width):
             old_pixel = pixels[x][y]
-            new_pixel = map_threshold(old_pixel)
+            new_pixel = similar_color(old_pixel)
+            pixels[x][y] = new_pixel
 
-            errors = [
-                old_color - new_color
-                for old_color, new_color
-                in zip(old_pixel, new_pixel)
-            ]
+            err = old_pixel - new_pixel
 
-            if x < width - 1:  # Pixel at East
-                pixel = [
-                    old_color + round(err * 7/16)
-                    for old_color, err
-                    in zip(pixels[x+1][y], errors)
-                ]
-                print(pixel)
-                pixels[x+1][y] = pixel
-
-            if x > 1 and y < height - 1:  # Pixel at South-West
-                pixel = [
-                    old_color + round(err * 3/16)
-                    for old_color, err
-                    in zip(pixels[x-1][y+1], errors)
-                ]
-                pixels[x-1][y+1] = pixel
-
-            if y < height - 1:  # Pixel at South
-                pixel = [
-                    old_color + round(err * 5/16)
-                    for old_color, err
-                    in zip(pixels[x][y+1], errors)
-                ]
-                pixels[x][y+1] = pixel
-
-            if x < width - 1 and y < height - 1:  # Pixel at South-East
-                pixel = [
-                    old_color + round(err / 16)
-                    for old_color, err
-                    in zip(pixels[x+1][y+1], errors)
-                ]
-                pixels[x+1][y+1] = pixel
+            pixels[x + 1][y] = pixels[x + 1][y] + err * 7 / 16
+            pixels[x - 1][y + 1] = pixels[x - 1][y + 1] + err * 3 / 16
+            pixels[x][y + 1] = pixels[x][y + 1] + err * 5 / 16
+            pixels[x + 1][y + 1] = pixels[x + 1][y + 1] + err * 1 / 16
 
     return pixels
 
 
-def map_threshold(pixel):
-    """Map to the closest colors"""
-    return [255 * floor(color/128) for color in pixel]
+def similar_color(old_pixel):
+    return round(old_pixel / 255, 2)
+
+
+if __name__ == '__main__':
+    m = [
+        [0, 0, 0],
+        [0, 1, 0],
+        [0, 0, 0]
+    ]
+    print(fs_dithering(m))
