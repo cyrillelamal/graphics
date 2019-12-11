c43e72a00cf3d23a0b8e1c144187f758154ae1a0
Lambda_man <cyrillekossyguine@outlook.fr>
2019-12-11 13:15:50 +0300

    vt in pw2 (3d-rotation)

--- pw2/vt1.py
+++ pw2/vt1.py
@@ -1,76 +1,46 @@
 """
-Transform triangle
+Rotate at x in 3D
 """
-import sys
-import tkinter as tk
+from math import sin, cos, radians
+from tkinter import *
 import numpy as np
+import sys
 
 sys.path.insert(0, '..')
-from converter import d2s, TRANSFORMATIONS as TR
+from converter import d2s
 
 
-CANVAS_SIZE = (640, 480)
-SCALE = (22, 22)
+PERIOD = 80  # ms
+CS = (200, 200)
+CW, CH = CS
 
-PERIOD = 1000  # ms
-SCENARIO = [
-    TR.get('y=x'),
-    TR.get('y=0'),
-]
+FIG = [(-10, -10, 0), (10, 10, 0),]
 
 
-# Counted constants
-CANVAS_WIDTH, CANVAS_HEIGHT = CANVAS_SIZE
-
-
-TRIANGLE = [
-    (8, 1),
-    (7, 3),
-    (6, 2)
-]
-
-
-def draw_triangle(points=None):
+def transform(a=0):
+    a = 0 if a == 360 else a
+    a_r = radians(a)
+    m = np.array(FIG)
+    t = np.array([
+        [1, 0, 0],
+        [0, cos(a_r), -sin(a_r)],
+        [0, sin(a_r), cos(a_r)]
+    ])
+    rotated = np.matmul(m, t).tolist()
+    points = []
+    for rp in rotated:
+        dp = list(d2s(rp, CS))
+        points += dp
+    print(a, points)
     canvas.delete('all')
-
-    points = TRIANGLE if points is None else points
-    num_of_points = len(points)
-
-    for i in range(num_of_points):
-        p1 = points[i]
-        p2 = points[0] if i == num_of_points - 1 else points[i+1]
-
-        p1 = d2s(p1, CANVAS_SIZE, SCALE)
-        p2 = d2s(p2, CANVAS_SIZE, SCALE)
-        canvas.create_line(*p1, *p2)
+    canvas.create_rectangle(*points)
+    canvas.after(PERIOD, transform, a+1)
 
 
-def transform(i=0):
-    if i == len(SCENARIO):
-        return
-    m = np.array(TRIANGLE)
-    t = np.array(SCENARIO[i])
-    points = np.matmul(m, t).tolist()
-
-    draw_triangle(points)
-
-    canvas.after(PERIOD, transform, i+1)
-
-
-MASTER = tk.Tk()
-MASTER.title('СР №2. ВЗ №1. Косыгин К.С.')
-
-# Frame with buttons
-fb = tk.Frame(MASTER)
-fb.pack()
-# Frame with canvas
-fc = tk.Frame(MASTER)
-fc.pack()
-
-# Canvas
-canvas = tk.Canvas(fc, width=CANVAS_WIDTH, height=CANVAS_HEIGHT)
+# Backend
+master = Tk()
+master.title('СЗ №2. ВСР №3')
+canvas = Canvas(master, width=CW, height=CH)
 canvas.pack()
-draw_triangle()
-
-MASTER.after(PERIOD, transform)
-MASTER.mainloop()
+master.after(PERIOD, transform)
+master.mainloop()
